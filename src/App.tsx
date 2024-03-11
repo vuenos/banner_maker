0080b09400c38d869970b8f0c7da81ff21849bc8
jintae kim <vuenos@naver.com>
2024-03-11 13:54:45 +0900

    Product vertion commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, {useEffect, useState, useRef, ChangeEvent} from "react";
 import { SketchPicker } from "react-color";
 import styled from "styled-components";
 import html2canvas from "html2canvas";
-import { Button, Input, Row, Col, Typography, Select } from "antd";
+import {Button, Input, Row, Col, Typography, Select} from "antd";
 import {
   BgColorsOutlined, CloudDownloadOutlined,
   ColumnHeightOutlined,
-  ColumnWidthOutlined,
+  ColumnWidthOutlined, ExclamationCircleOutlined,
   FontColorsOutlined,
-  FontSizeOutlined, MessageOutlined
+  FontSizeOutlined, MessageOutlined, PictureOutlined
 } from "@ant-design/icons";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 const Swatch = styled.div`
-    height: 24px;
-    padding: 8px 8px;
+    height: 28px;
+    padding: 6px 6px;
     background: #fff;
-    border-radius: 6px;
+    border-radius: 8px;
     box-shadow: 0 0 0 1px rgba(0,0,0,.1);
     display: flex;
     cursor: pointer;
@@ -27,8 +27,8 @@ const Swatch = styled.div`
 
 const PopOver = styled.div`
     position: absolute;
-    top: 40px;
-    right: 0;
+    top: -20px;
+    left: 16px;
     z-index: 2;  
   `;
 
@@ -43,18 +43,21 @@ const Cover = styled.div`
 function App() {
 
   const [text, setText] = useState("Sample Text");
-  const [width, setWidth] = useState("640");
-  const [height, setHeight] = useState("320");
+  const [width, setWidth] = useState<string>("960");
+  const [height, setHeight] = useState<string>("540");
   const [color, setColor] = useState("green");
   const [fontColor, setFontColor] = useState("#FFFFFF");
   const [fontSize, setFontSize] = useState("50");
   const [fontFamily, setFontFamily] = useState<string>("East Sea Dokdo");
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
   const [displayBgColorPicker, setDisplayBgColorPicker] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>("")
 
   const inputRef = useRef(null);
+  const inputRefWidth = useRef(null);
   const canvasRef = useRef<HTMLDivElement>(null)
   const editorRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
 
   const arrRandomWordings = [
     "독도는 우리땅",
@@ -90,7 +93,11 @@ function App() {
   }
 
   const colorChange = (color: { hex: string }) => {
-    setColor(color.hex)
+    const canvasContainer = document.getElementById("canvasContainer");
+    if (canvasContainer) {
+      canvasContainer.style.backgroundImage = "none";
+    }
+    setColor(color.hex);
   }
 
   const fontColorChange = (color: { hex: string }) => {
@@ -102,6 +109,10 @@ function App() {
   }
 
   const getRandomColor = () => {
+    const canvasContainer = document.getElementById("canvasContainer");
+    if (canvasContainer) {
+      canvasContainer.style.backgroundImage = "none";
+    }
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
 
@@ -117,6 +128,16 @@ function App() {
     }
   };
 
+  const ratioChangeHandler = (w: string, h: string) => {
+    setWidth(w);
+    setHeight(h);
+  };
+
+  const ratioZoomHandler = (ratio: number) => {
+    setWidth((prevWidth) => prevWidth * ratio);
+    setHeight((prevHeight) => prevHeight * ratio);
+  }
+
   const downloadImage = async () => {
     const canvasContainer = canvasRef.current as HTMLDivElement;
     const canvas = await html2canvas(canvasContainer);
@@ -128,6 +149,21 @@ function App() {
     link.download = 'banner-sample.png';
     link.click();
   };
+
+  const imageUploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const result = reader.result as string;
+        setImageUrl(result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 
   const handleClick = () => {
     setDisplayColorPicker(true)
@@ -187,141 +223,181 @@ function App() {
 
   return (
     <>
-      <Row justify="center" style={{ marginTop: "-120px" }}>
-        <Title id="page-title" level={1}>Banner created by entering text</Title>
+      <Row justify="center">
+        <Title level={1} style={{ marginBlock: 0, fontSize: 0, height: 0 }}>
+          You can create YouTube thumbnails, TikTok thumbnails, and YouTube Shorts thumbnails for free.
+        </Title>
+        <Title id="page-title" style={{ marginTop: 0 }} level={2}>Banner created by entering text</Title>
       </Row>
 
-      <Row justify="center" style={{ marginBottom: "24px" }}>
-        <Col>
-          <div
-              id="canvasContainer"
-              ref={canvasRef}
-              style={{
-                fontSize: `${fontSize}px`,
-                width: `${width}px`,
-                height: `${height}px`,
-              }}
-          >
-            <div
-                id="editor"
-                ref={editorRef}
-                contentEditable="true"
-                spellCheck="false"
+      <Row justify="center" gutter={24}>
+
+        <Col flex="240px">
+          <Row>
+            <Input
+              ref={inputRefWidth}
+              addonBefore={<ColumnWidthOutlined />}
+              addonAfter="px"
+              size="large"
+              type="number"
+              name="width"
+              value={width}
+              onChange={widthChange}
+            />
+            <Input
+              ref={inputRef}
+              addonBefore={<ColumnHeightOutlined />}
+              addonAfter="px"
+              size="large"
+              type="number"
+              name="height"
+              value={height}
+              onChange={heightChange}
+              style={{ marginTop: "16px" }}
+            />
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Button type="default" size="small" onClick={() => ratioChangeHandler("960", "540")}>16 : 9</Button>
+            <Button type="default" size="small" onClick={() => ratioChangeHandler("360", "640")} style={{ marginLeft: "8px" }}>9 : 16</Button>
+            <Button type="default" size="small" onClick={() => ratioChangeHandler("1280", "720")} style={{ marginLeft: "8px" }}>Youtube</Button>
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(0.75)}>x 0.75</Button>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(0.5)} style={{ marginLeft: "8px" }}>x 0.5</Button>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(0.25)} style={{ marginLeft: "8px" }}>x 0.25</Button>
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(1.25)}>x 1.25</Button>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(1.5)} style={{ marginLeft: "8px" }}>x 1.5</Button>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(1.75)} style={{ marginLeft: "8px" }}>x 1.75</Button>
+            <Button type="default" size="small" onClick={() => ratioZoomHandler(2)} style={{ marginLeft: "8px" }}>x 2</Button>
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Select
+              className="font-select"
+              size="large"
+              defaultValue="East Sea Dokdo"
+              style={{ width: "100%" }}
+              onChange={handleSelectChange}
+              options={[
+                { value: 'Song Myung', label: 'Song Myung', className: "ff-song-myung"},
+                { value: 'Yeon Sung', label: 'Yeon Sung', className: "ff-yeon-sung" },
+                { value: 'Noto Sans KR', label: 'Noto Sans KR', className: "ff-noto-kr"},
+                { value: 'Noto Serif KR', label: 'Noto Serif KR', className: "ff-noto-serif" },
+                { value: 'East Sea Dokdo', label: 'East Sea Dokdo', className: "ff-dokdo" },
+                { value: 'Single Day', label: 'Single Day', className: "ff-single-day" },
+                { value: 'Nanum Pen Script', label: 'Nanum Pen Script', className: "ff-nanum-pen" },
+                { value: 'PT Serif', label: 'PT Serif', className: "ff-PT-serif" },
+                { value: 'Roboto', label: 'Roboto', className: "ff-roboto" },
+                { value: 'Nunito', label: 'Nunito', className: "ff-nunito" },
+                { value: 'Lobster', label: 'Lobster', className: "ff-lobster" },
+                { value: 'Bebas Neue', label: 'Bebas Neue', className: "ff-bebas-neue" },
+              ]}
+            />
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Input
+              ref={inputRef}
+              addonBefore={<FontSizeOutlined />}
+              addonAfter="px"
+              size="large"
+              type="number"
+              name="fontSize"
+              defaultValue={fontSize}
+              onChange={fontSizeChange}
+            />
+          </Row>
+
+          <Row style={{ marginTop: "16px" }}>
+            <Swatch onClick={ handleBgClick }>
+              <BgColorsOutlined />
+              <div
+                className="colorChip"
+                style={{
+                  backgroundColor: color
+                }}
+              >
+              </div>
+            </Swatch>
+            { displayBgColorPicker ? <PopOver>
+              <Cover onClick={ handleClose } />
+              <SketchPicker color={ color } onChange={ colorChange } />
+            </PopOver> : null }
+
+            <Swatch onClick={ handleClick } style={{ marginLeft: "8px" }}>
+              <FontColorsOutlined />
+              <div
+                className="colorChip"
+                style={{
+                  backgroundColor: fontColor
+                }}
+              >
+              </div>
+            </Swatch>
+            { displayColorPicker ? <PopOver>
+              <Cover onClick={ handleClose } />
+              <SketchPicker color={ fontColor } onChange={ fontColorChange } />
+            </PopOver> : null }
+            <Button
+              size="large"
+              onClick={() => imageRef.current?.click()}
+              style={{ marginLeft: "8px" }}
             >
-            </div>
-          </div>
-        </Col>
-      </Row>
+              <PictureOutlined className="uploadPhoto" />
+            </Button>
+            <input
+              ref={imageRef}
+              type="file"
+              accept="image/*"
+              onChange={imageUploadHandler}
+              style={{ display: "none" }}
+            />
+          </Row>
 
-      <Row justify="center" gutter={20} style={{ marginBottom: "24px" }}>
-        <Col span={4}>
-          <Select
-            className="font-select"
-            size="large"
-            defaultValue="East Sea Dokdo"
-            style={{ width: "100%" }}
-            onChange={handleSelectChange}
-            options={[
-              { value: 'Song Myung', label: 'Song Myung', className: "ff-song-myung"},
-              { value: 'Yeon Sung', label: 'Yeon Sung', className: "ff-yeon-sung" },
-              { value: 'Noto Sans KR', label: 'Noto Sans KR', className: "ff-noto-kr"},
-              { value: 'Noto Serif KR', label: 'Noto Serif KR', className: "ff-noto-serif" },
-              { value: 'East Sea Dokdo', label: 'East Sea Dokdo', className: "ff-dokdo" },
-              { value: 'Single Day', label: 'Single Day', className: "ff-single-day" },
-              { value: 'Nanum Pen Script', label: 'Nanum Pen Script', className: "ff-nanum-pen" },
-              { value: 'PT Serif', label: 'PT Serif', className: "ff-PT-serif" },
-              { value: 'Roboto', label: 'Roboto', className: "ff-roboto" },
-              { value: 'Nunito', label: 'Nunito', className: "ff-nunito" },
-              { value: 'Lobster', label: 'Lobster', className: "ff-lobster" },
-              { value: 'Bebas Neue', label: 'Bebas Neue', className: "ff-bebas-neue" },
-            ]}
-          />
+          <Row style={{ marginTop: "16px" }}>
+            <Button type="primary" size="large" shape="round" block style={{ marginTop: "16px" }} icon={<CloudDownloadOutlined />} onClick={downloadImage}>Download</Button>
+            <Button type="default" size="large" shape="round" block style={{ marginTop: "16px" }} icon={<BgColorsOutlined />} onClick={() => randomColor()}>Random</Button>
+            <Button type="default" size="large" shape="round" block style={{ marginTop: "16px" }} icon={<MessageOutlined />} onClick={() => handleRandomWording()}>Random Words</Button>
+          </Row>
         </Col>
 
-        <Col span={4}>
-          <Input
-            ref={inputRef}
-            addonBefore={<FontSizeOutlined />}
-            addonAfter="px"
-            size="large"
-            type="number"
-            name="fontSize"
-            defaultValue={fontSize}
-            onChange={fontSizeChange}
-          />
+        <Col span={16}>
+          <Row>
+            <Col>
+              <div
+                id="canvasContainer"
+                ref={canvasRef}
+                style={{
+                  fontSize: `${fontSize}px`,
+                  width: `${width}px`,
+                  height: `${height}px`,
+                  backgroundImage: `url(${imageUrl})`,
+                  backgroundRepeat: 'none',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              >
+                <div
+                  id="editor"
+                  ref={editorRef}
+                  contentEditable="true"
+                  spellCheck="false"
+                >
+                </div>
+              </div>
+
+              <Text type="secondary" style={{ display: "block", marginTop: "8px", color: "#AAA", textAlign: "right" }}>
+                <ExclamationCircleOutlined /> Click on the text and edit it
+              </Text>
+            </Col>
+          </Row>
         </Col>
 
-        <Col span={4}>
-          <Input
-            ref={inputRef}
-            addonBefore={<ColumnWidthOutlined />}
-            addonAfter="px"
-            size="large"
-            type="number"
-            name="width"
-            defaultValue={width}
-            onChange={widthChange}
-          />
-        </Col>
-
-        <Col span={4}>
-          <Input
-            ref={inputRef}
-            addonBefore={<ColumnHeightOutlined />}
-            addonAfter="px"
-            size="large"
-            type="number"
-            name="height"
-            defaultValue={height}
-            onChange={heightChange}
-          />
-        </Col>
-
-        <Col>
-          <Swatch onClick={ handleBgClick }>
-            <BgColorsOutlined />
-            <div
-              className="colorChip"
-              style={{
-                backgroundColor: color
-              }}
-            >
-            </div>
-          </Swatch>
-          { displayBgColorPicker ? <PopOver>
-            <Cover onClick={ handleClose } />
-            <SketchPicker color={ color } onChange={ colorChange } />
-          </PopOver> : null }
-        </Col>
-
-        <Col>
-          <Swatch onClick={ handleClick }>
-            <FontColorsOutlined />
-            <div
-              className="colorChip"
-              style={{
-                backgroundColor: fontColor
-              }}
-            >
-            </div>
-          </Swatch>
-          { displayColorPicker ? <PopOver>
-            <Cover onClick={ handleClose } />
-            <SketchPicker color={ fontColor } onChange={ fontColorChange } />
-          </PopOver> : null }
-        </Col>
-      </Row>
-
-      <Row justify="center" gutter={16}>
-        <Col>
-          <Button type="primary" size="large" shape="round" icon={<CloudDownloadOutlined />} onClick={downloadImage}>Download</Button>
-        </Col>
-        <Col>
-          <Button type="default" size="large" shape="round" icon={<BgColorsOutlined />} onClick={() => randomColor()}>Random</Button>
-        </Col>
-        <Col>
-          <Button type="default" size="large" shape="round" icon={<MessageOutlined />} onClick={() => handleRandomWording()}>Random Words</Button>
-        </Col>
       </Row>
     </>
   )
